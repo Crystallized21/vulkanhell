498e5220d0765e1a8c3c67a2347f16ec1264f6c5
Michael Bui <michael.bui_crystal@outlook.com>
2025-04-24 17:09:52 +1200

    add `intiwindow()` method

--- LearnVulkan/main.cpp
+++ LearnVulkan/main.cpp
@@ -16,12 +16,24 @@ const uint32_t HEIGHT = 600;
 class HelloTriangleApplication {
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    GLFWwindow* window;
+
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
     void initVulkan() {
 
     }
