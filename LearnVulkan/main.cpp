3c31b099926edc29ae511eb4a779c9c4752bec96
Michael Bui <michael.bui_crystal@outlook.com>
2025-04-25 23:58:20 +1200

    query for presentation support

--- LearnVulkan/main.cpp
+++ LearnVulkan/main.cpp
@@ -62,9 +62,10 @@ void DestroyDebugUtilsMessengerEXT
 
 struct QueueFamilyIndices {
     std::optional<uint32_t> graphicsFamily;
+    std::optional<uint32_t> presentFamily>;
 
     bool isComplete() {
-        return graphicsFamily.has_value();
+        return graphicsFamily.has_value() && presentFamily.has_value();
     }
 };
 
@@ -307,6 +308,13 @@ private:
         for (const auto &queueFamily: queueFamilies) {
             if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
                 indices.graphicsFamily = i;
+            }
+
+            VkBool32 presentSupport = false;
+            vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+
+            if (presentSupport) {
+                indices.presentFamily = i;
             }
 
             if (indices.isComplete()) {
